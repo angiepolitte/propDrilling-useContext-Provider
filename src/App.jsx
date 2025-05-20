90c2d8f5640bb97cc4568692d8441d440e5f6a31
Angie <angiepolitte@gmail.com>
2025-05-19 20:16:27 -0500

    provider

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import "./App.css";
 const ThemeContext = createContext("light");
 function App() {
   return (
-    <div style={{ border: "2px solid yellow", padding: "20px" }}>
-      <h2>App (Parent)</h2>
-      <ComponentA />
-    </div>
+    <ThemeContext.Provider value="dark">
+      <div style={{ border: "2px solid yellow", padding: "20px" }}>
+        <h2>App (Parent)</h2>
+        <ComponentA />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
@@ -91,3 +93,43 @@ export default App;
 // // function ThemedComponent({ theme }) {
 // //   return <div>The current theme is: {theme}</div>;
 // // }
+
+// //  useContext
+
+// const ThemeContext = createContext("light");
+// function App() {
+//   return (
+//     <div style={{ border: "2px solid yellow", padding: "20px" }}>
+//       <h2>App (Parent)</h2>
+//       <ComponentA />
+//     </div>
+//   );
+// }
+
+// function ComponentA() {
+//   return (
+//     <div style={{ border: "2px solid blue", padding: "20px" }}>
+//       <h2>ComponentA (Child)</h2>
+//       <ComponentB />
+//     </div>
+//   );
+// }
+
+// function ComponentB() {
+//   return (
+//     <div style={{ border: "2px solid pink", padding: "20px" }}>
+//       <h2>ComponentB (GrandChild)</h2>
+//       <ThemedComponent />
+//     </div>
+//   );
+// }
+
+// function ThemedComponent() {
+//   const theme = useContext(ThemeContext);
+//   return (
+//     <div style={{ border: "2px solid green", padding: "20px" }}>
+//       <h2>ThemedComponent (Great-GrandChild)</h2>
+//       <div>The current theme is: {theme}</div>
+//     </div>
+//   );
+// }
